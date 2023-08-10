800f7044949a9c2f65b89b98dfe3a7453db302ac
tvinchent <tvinchent@gmail.com>
2023-08-10 21:46:14 +0200

    add some TU

--- game.spec.js
+++ game.spec.js
@@ -1,21 +1,36 @@
 require('./game.js');
 
-describe("The test environment", function() {
+describe('Game', () => {
+  let game;
 
-  it("should access game", function() {
-    expect(Game).toBeDefined();
+  beforeEach(() => {
+    game = new Game();
+  });
+
+  it('should add a player', () => {
+    game.add('Player 1');
+    expect(game.howManyPlayers()).toBe(1);
+  });
+
+  it('should not add more than the maximum number of players', () => {
+    for (let i = 1; i <= 6; i++) {
+      game.add(`Player ${i}`);
+    }
+    expect(game.add('Extra Player')).toBe(false);
+  });
+
+  it('should determine if the game is playable with at least 2 players', () => {
+    game.add('Player 1');
+    expect(game.isPlayable()).toBe(false);
+
+    game.add('Player 2');
+    expect(game.isPlayable()).toBe(true);
+  });
+
+  it('should correctly roll the dice and update player location', () => {
+    game.add('Player 1');
+    game.roll(4);
+    expect(game.places[0]).toBe(4);
   });
 
 });
-
-describe("isPlayable", function() {
-
-  it("should pass", function() {
-    var isPlayable = this.isPlayable()
-    expect(isPlayable).toBe(true);
-  });
-
-});
-
-  // it should work with 2 to 6 (or more) players
-  // it should display an alert when less than 2 players
